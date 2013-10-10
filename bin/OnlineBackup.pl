2bf64571e9c164c42625d80a24fdad1e60da62fb
patklaey <pat.klaey@stepping-stone.ch>
2013-10-10 11:28:00 +0200

    Preparing for release 2.0.3

--- bin/OnlineBackup.pl
+++ bin/OnlineBackup.pl
@@ -46,7 +46,7 @@ push (@INC, $basedir);
 require OLBUtils;
 
 # Version
-use constant VERSION => "2.0.3_pre";
+use constant VERSION => "2.0.3";
 
 # setting default values
 my $configfile = "../conf/OnlineBackup.conf";
